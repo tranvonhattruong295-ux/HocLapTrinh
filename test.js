a5026b592b5b205f2c8214864706def16ad0b3bf
tranvonhattruong295-ux <tranvonhattruong295@gmail.com>
2026-07-23 16:08:41 +0700

    First commit

---
+++ test.js
@@ -0,0 +1,10 @@
+let color="mauxanh";
+if (color=="mauxanh"){
+    console.log("Day la mau xanh");
+} else if( color== "maudo"){
+    console.log("Day la mau do");
+} else if( color=="mauvang"){
+    console.log("Day la mau vang");
+}else{
+    console.log("Cac mau khac");
+}
